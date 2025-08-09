a7fe7a78534460f58ef3a02fea18488a15d952b1
Ronaldo Ferreira De Novaes <333173@grupofolha.intranet>
2025-08-09 19:30:57 -0300

    static ads urls curta ads

--- internal/tenant/tenant.go
+++ internal/tenant/tenant.go
@@ -10,10 +10,10 @@ type Tenant struct {
 }
 
 var tenants = map[string]Tenant{
-	"conexaoguarulhos.com.br": {ID: 1, Portal: "conexaoguarulhos.com.br", AdsURL: "https://conexao.gru.br", Static: "https://static.conexao.gru.br"},
-	"www.conexaoguarulhos.com.br": {ID: 1, Portal: "conexaoguarulhos.com.br", AdsURL: "https://conexao.gru.br", Static: "https://static.conexao.gru.br"},
-	"gazetadeosasco.com.br": {ID: 2, Portal: "gazetadeosasco.com.br", AdsURL: "https://gazeta.osasco.br", Static: "https://static.gazeta.osasco.br"},
-	"www.gazetadeosasco.com.br": {ID: 2, Portal: "gazetadeosasco.com.br", AdsURL: "https://gazeta.osasco.br", Static: "https://static.gazeta.osasco.br"},
+	"conexaoguarulhos.com.br": {ID: 1, Portal: "conexaoguarulhos.com.br", AdsURL: "https://ads.conexao.gru.br", Static: "https://static.conexao.gru.br"},
+	"www.conexaoguarulhos.com.br": {ID: 1, Portal: "conexaoguarulhos.com.br", AdsURL: "https://ads.conexao.gru.br", Static: "https://static.conexao.gru.br"},
+	"gazetadeosasco.com.br": {ID: 2, Portal: "gazetadeosasco.com.br", AdsURL: "https://ads.gazeta.osasco.br", Static: "https://static.gazeta.osasco.br"},
+	"www.gazetadeosasco.com.br": {ID: 2, Portal: "gazetadeosasco.com.br", AdsURL: "https://ads.gazeta.osasco.br", Static: "https://static.gazeta.osasco.br"},
 }
 
 var Default = Tenant{ID: 1, Portal: "conexaoguarulhos.com.br", AdsURL: "https://conexao.gru.br", Static: "https://static.conexao.gru.br"}
